5422db762dc3799624adbabd7ed2e1e3dd0efc44
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-01-14 23:06:29 +0530

    Initial commit

---
+++ src/component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Component() {
+  return (
+    <div>
+    <h2> Counter using React</h2>
+    Your current count is : 
+    <hr/>
+    <div>error</div>
+    <button >Decrement</button> &nbsp;&nbsp;
+    <button >Increment</button>&nbsp;&nbsp; 
+    <button >Go Back to 0</button>
+    </div>
+  )
+}
+
+export default Component;
